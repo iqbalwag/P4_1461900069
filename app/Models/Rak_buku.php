d3b6b71495f9e0d5952c0ab1f2e925ae2172ee25
iqbalwag <achiqbalp@gmail.com>
2021-06-12 12:25:53 +0700

    Pertama

---
+++ app/Models/Rak_buku.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Rak_buku extends Model
+{
+    protected $table = "rak_buku";
+ 
+    protected $fillable = ['id', 'id_buku', 'id_jenis_buku'];
+}
---
+++ resources/views/home0069.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HOME</title>
+</head>
+<style>
+.button {
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+
+.button1 {border: 2px solid #000000; background-color: #FFFFFF; color: black;}
+.button2 {background-color: #000000; color: white;}
+</style>
+<body>
+	<div class="container-md" align="center">
+  		<h1>HOME</h1>
+  		<td align="center"><button onclick="document.location='/buku'" class="button button1">Buku</button>
+	</div>
+</body>
+</html>
